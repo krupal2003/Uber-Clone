7055665b9fa0bd5aa4ab18502837ad0b991816a4
Krupal Patel <krupalpatel18102003@gmail.com>
2025-06-18 01:08:00 +0530

    intialize frontend and create basic setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
+import React from 'react'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignUp from './pages/UserSignUp'
+import CaptainLogin from './pages/CaptainLogin' 
+import CaptainSignUp from './pages/CaptainSignUp'
+
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/user-signup' element={<UserSignUp></UserSignUp>}></Route>
+        <Route path='/user-login' element={<UserLogin></UserLogin>}></Route>
+        <Route path='/captain-signup' element={<CaptainSignUp></CaptainSignUp>}></Route>
+        <Route path='/captain-login' element={<CaptainLogin></CaptainLogin>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
